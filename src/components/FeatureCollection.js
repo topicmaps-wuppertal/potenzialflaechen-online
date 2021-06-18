23060e2ac129a6957a2a3adfe1f2bfdb0539f623
helllth <th@cismet.de>
2021-06-18 16:57:27 +0200

    added md5 based DAQ caching (temp. locally, later fromn react-cismap)

--- src/components/FeatureCollection.js
+++ src/components/FeatureCollection.js
@@ -2,6 +2,96 @@ import { useContext, useEffect } from "react";
 import { FeatureCollectionDispatchContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
 import { TopicMapDispatchContext } from "react-cismap/contexts/TopicMapContextProvider";
 import FeatureCollection from "react-cismap/FeatureCollection";
+import localforage from "localforage";
+//import { md5ActionFetchDAQ } from "react-cismap/tools/fetching";
+const md5ActionFetchDAQ = async (prefix, apiUrl, jwt, daqKey) => {
+  const cachePrefix = "@" + prefix + ".." + apiUrl + "." + daqKey;
+  const md5Key = cachePrefix + ".md5";
+  const dataKey = cachePrefix + ".data";
+  const timeKey = cachePrefix + ".time";
+  const md5InCache = await localforage.getItem(md5Key);
+  console.log("DAQ for " + daqKey);
+  let taskParameters = {
+    parameters: {
+      daqKey,
+      md5: md5InCache || "-",
+    },
+  };
+
+  let fd = new FormData();
+  fd.append(
+    "taskparams",
+    new Blob([JSON.stringify(taskParameters)], {
+      type: "application/json",
+    })
+  );
+
+  const response = await fetch(
+    apiUrl + "/actions/WUNDA_BLAU.dataAquisition/tasks?resultingInstanceType=result",
+    {
+      method: "POST",
+      // method: "GET",
+      headers: {
+        Authorization: "Bearer " + jwt,
+        // "Content-Type": "application/json",
+        // Accept: "application/json",
+      },
+      body: fd,
+    }
+  );
+
+  if (response.status >= 200 && response.status < 300) {
+    const content = await response.json();
+    if (content.res) {
+      try {
+        const result = JSON.parse(content.res);
+        let status = result.status;
+        let potenzialflaechen;
+        if (status === 200) {
+          console.log("DAQ cache miss for " + daqKey);
+
+          potenzialflaechen = JSON.parse(result.content);
+          await localforage.setItem(dataKey, result.content);
+          await localforage.setItem(md5Key, result.md5);
+          await localforage.setItem(timeKey, result.time);
+        } else if (status === 304) {
+          console.log("DAQ cache hit for " + daqKey);
+
+          potenzialflaechen = JSON.parse(await localforage.getItem(dataKey));
+        }
+
+        return new Promise((resolve, reject) => {
+          resolve(potenzialflaechen);
+        });
+      } catch (e) {
+        return new Promise((resolve, reject) => {
+          reject({
+            status: 500,
+            desc: "error when parsing the server result. probably the content has the wrong structure",
+            content,
+            exception: e,
+          });
+        });
+      }
+    } else {
+      return new Promise((resolve, reject) => {
+        reject({
+          status: 500,
+          desc: "error when parsing the server result.",
+          content,
+        });
+      });
+    }
+  } else if (response.status === 401) {
+    return new Promise((resolve, reject) => {
+      reject({ status: response.status, desc: "unauthorized" });
+    });
+  } else {
+    return new Promise((resolve, reject) => {
+      reject({ status: response.status, desc: "unknown" });
+    });
+  }
+};
 
 const FC = ({ jwt, setJWT, setLoginInfo }) => {
   const { zoomToFeature } = useContext(TopicMapDispatchContext);
@@ -9,71 +99,29 @@ const FC = ({ jwt, setJWT, setLoginInfo }) => {
 
   useEffect(() => {
     if (jwt !== undefined) {
-      let taskParameters = {
-        parameters: {
-          daqKey: "potenzialflaechen",
-          md5: "undefined", ///"----29a21a62f8973ab1baa78b7fe9320d10",
-        },
-      };
-
-      let fd = new FormData();
-      fd.append(
-        "taskparams",
-        new Blob([JSON.stringify(taskParameters)], {
-          type: "application/json",
-        })
-      );
-      fetch(
-        // "https://potenzialflaechen-online-api.cismet.de/users",
-        // "https://eniptvf9euuoeze.m.pipedream.net",
-
-        "https://potenzialflaechen-online-api.cismet.de/actions/WUNDA_BLAU.dataAquisition/tasks?resultingInstanceType=result",
-        {
-          method: "POST",
-          // method: "GET",
-          headers: {
-            Authorization: "Bearer " + jwt,
-            // "Content-Type": "application/json",
-            // Accept: "application/json",
-          },
-          body: fd,
-        }
+      md5ActionFetchDAQ(
+        "potenzialflaechen-online",
+        "https://potenzialflaechen-online-api.cismet.de",
+        jwt,
+        "potenzialflaechen"
       )
-        .then(function (response) {
-          if (response.status >= 200 && response.status < 300) {
-            response.json().then(function (content) {
-              // console.log("response from cids", content);
-              if (content.res) {
-                const result = JSON.parse(content.res);
-                let potenzialflaechen;
-
-                if (Array.isArray(result)) {
-                  potenzialflaechen = result;
-                } else {
-                  console.log("result", result);
-
-                  potenzialflaechen = JSON.parse(result.content);
-                }
-                setItems(potenzialflaechen);
-              } else {
-                console.log("Probleme ber der Abfrage der Daten");
-              }
-            });
-          } else {
-            if (response.status === 401) {
+        .then(
+          (potenzialflaechen) => {
+            setItems(potenzialflaechen);
+          },
+          (problem) => {
+            if (problem.status === 401) {
               setJWT(undefined);
               setLoginInfo({ color: "#F9D423", text: "Bitte melden Sie sich erneut an." });
               setTimeout(() => {
                 setLoginInfo();
               }, 2500);
             }
-            // response.text().then((x) => {189	60	90
-            //   console.log("error", x);
-            // });
+            setItems([]);
           }
-        })
-        .catch(function (err) {
-          ///
+        )
+        .catch((e) => {
+          console.log("xxx error ", e);
         });
     } else {
       setItems([]);
