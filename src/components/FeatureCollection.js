32a80d64b86e6cb070c14f9c6fa7dc221381888f
helllth <th@cismet.de>
2021-06-22 10:56:51 +0200

    switched to md5ActionFetchDAQ for data

--- src/components/FeatureCollection.js
+++ src/components/FeatureCollection.js
@@ -1,100 +1,15 @@
 import { useContext, useEffect } from "react";
 import { FeatureCollectionDispatchContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
-import { TopicMapDispatchContext } from "react-cismap/contexts/TopicMapContextProvider";
+import {
+  TopicMapContext,
+  TopicMapDispatchContext,
+} from "react-cismap/contexts/TopicMapContextProvider";
 import FeatureCollection from "react-cismap/FeatureCollection";
-import localforage from "localforage";
-//import { md5ActionFetchDAQ } from "react-cismap/tools/fetching";
-const md5ActionFetchDAQ = async (prefix, apiUrl, jwt, daqKey) => {
-  const cachePrefix = "@" + prefix + ".." + apiUrl + "." + daqKey;
-  const md5Key = cachePrefix + ".md5";
-  const dataKey = cachePrefix + ".data";
-  const timeKey = cachePrefix + ".time";
-  const md5InCache = await localforage.getItem(md5Key);
-  console.log("DAQ for " + daqKey);
-  let taskParameters = {
-    parameters: {
-      daqKey,
-      md5: md5InCache || "-",
-    },
-  };
-
-  let fd = new FormData();
-  fd.append(
-    "taskparams",
-    new Blob([JSON.stringify(taskParameters)], {
-      type: "application/json",
-    })
-  );
-
-  const response = await fetch(
-    apiUrl + "/actions/WUNDA_BLAU.dataAquisition/tasks?resultingInstanceType=result",
-    {
-      method: "POST",
-      // method: "GET",
-      headers: {
-        Authorization: "Bearer " + jwt,
-        // "Content-Type": "application/json",
-        // Accept: "application/json",
-      },
-      body: fd,
-    }
-  );
-
-  if (response.status >= 200 && response.status < 300) {
-    const content = await response.json();
-    if (content.res) {
-      try {
-        const result = JSON.parse(content.res);
-        let status = result.status;
-        let potenzialflaechen;
-        if (status === 200) {
-          console.log("DAQ cache miss for " + daqKey);
-
-          potenzialflaechen = JSON.parse(result.content);
-          await localforage.setItem(dataKey, result.content);
-          await localforage.setItem(md5Key, result.md5);
-          await localforage.setItem(timeKey, result.time);
-        } else if (status === 304) {
-          console.log("DAQ cache hit for " + daqKey);
-
-          potenzialflaechen = JSON.parse(await localforage.getItem(dataKey));
-        }
-
-        return new Promise((resolve, reject) => {
-          resolve(potenzialflaechen);
-        });
-      } catch (e) {
-        return new Promise((resolve, reject) => {
-          reject({
-            status: 500,
-            desc: "error when parsing the server result. probably the content has the wrong structure",
-            content,
-            exception: e,
-          });
-        });
-      }
-    } else {
-      return new Promise((resolve, reject) => {
-        reject({
-          status: 500,
-          desc: "error when parsing the server result.",
-          content,
-        });
-      });
-    }
-  } else if (response.status === 401) {
-    return new Promise((resolve, reject) => {
-      reject({ status: response.status, desc: "unauthorized" });
-    });
-  } else {
-    return new Promise((resolve, reject) => {
-      reject({ status: response.status, desc: "unknown" });
-    });
-  }
-};
+import { md5ActionFetchDAQ } from "react-cismap/tools/fetching";
 
 const FC = ({ jwt, setJWT, setLoginInfo }) => {
   const { zoomToFeature } = useContext(TopicMapDispatchContext);
+  const { routedMapRef } = useContext(TopicMapContext);
   const { setItems } = useContext(FeatureCollectionDispatchContext);
 
   useEffect(() => {
@@ -142,9 +57,22 @@ const FC = ({ jwt, setJWT, setLoginInfo }) => {
         const feature = event.target.feature;
 
         if (feature.selected === true) {
-          // console.re.log(nonce + "yyy will zoom");
+          // const before = JSON.stringify(routedMapRef.leafletMap.leafletElement.getBounds());
 
+          // const checkAfterBoundsChanged = (e) => {
+          //   console.log("e", e);
+
+          //   const after = JSON.stringify(routedMapRef.leafletMap.leafletElement.getBounds());
+          //   if (after === before) {
+
+          //   }
+          // };
+          // routedMapRef.leafletMap.leafletElement.on("moveend", checkAfterBoundsChanged);
+          // // routedMapRef.leafletMap.leafletElement.on("zoomend", checkAfterBoundsChanged);
           zoomToFeature(event.target.feature);
+
+          // routedMapRef.leafletMap.leafletElement.off("moveend", checkAfterBoundsChanged);
+          // // routedMapRef.leafletMap.leafletElement.off("zoomend", checkAfterBoundsChanged);
         }
         // }
       }}
