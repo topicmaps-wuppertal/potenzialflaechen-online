36ac56012d1627b8e0f5055eed32d0e708a3d1b5
helllth <th@cismet.de>
2021-07-13 09:51:55 +0200

    detect sibblings after loading the items

--- src/components/FeatureCollection.js
+++ src/components/FeatureCollection.js
@@ -1,21 +1,20 @@
-import { useContext, useEffect } from "react";
-import { FeatureCollectionDispatchContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
-import {
-  TopicMapContext,
-  TopicMapDispatchContext,
-} from "react-cismap/contexts/TopicMapContextProvider";
-import FeatureCollection from "react-cismap/FeatureCollection";
-import { md5ActionFetchDAQ } from "react-cismap/tools/fetching";
+import area from "@turf/area";
+import intersect from "@turf/intersect";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
-import { appKey } from "../PotenzialflaechenOnlineMap";
+import { useContext, useEffect } from "react";
+import { FeatureCollectionDispatchContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
+import { TopicMapDispatchContext } from "react-cismap/contexts/TopicMapContextProvider";
+import FeatureCollection from "react-cismap/FeatureCollection";
+import { md5ActionFetchDAQ } from "react-cismap/tools/fetching";
+import { createFlatbushIndex, findInFlatbush } from "react-cismap/tools/gisHelper";
 import { apiUrl, dataDaqKey } from "../App";
-
+import { appKey } from "../PotenzialflaechenOnlineMap";
+import convertItemToFeature from "../utils/convertItemToSimpleFeature";
 dayjs.extend(customParseFormat);
 
 const FC = ({ jwt, setJWT, setLoginInfo }) => {
   const { zoomToFeature } = useContext(TopicMapDispatchContext);
-  const { routedMapRef } = useContext(TopicMapContext);
   const { setItems, setMetaInformation } = useContext(FeatureCollectionDispatchContext);
 
   useEffect(() => {
@@ -25,6 +24,53 @@ const FC = ({ jwt, setJWT, setLoginInfo }) => {
           (result) => {
             //parse e.g. 2021-06-23 06:51:56.97500
             const time = dayjs(result.time, "YYYY-MM-DD hh:mm:ss").toDate();
+
+            //Check for sibblings and add them to the original data
+            // console.time("xxx sibblingsCheck");
+            const features = [];
+            for (const item of result.data) {
+              features.push(convertItemToFeature(item));
+            }
+            const polyIndex = createFlatbushIndex(features);
+
+            let i = 0;
+            for (const feature of features) {
+              const hits = findInFlatbush(polyIndex, feature, features, (hit) => {
+                return hit.id !== feature.id;
+              });
+              const overlapping = [];
+              if (hits.length > 0) {
+                const featureArea = area(feature);
+                for (const hit of hits) {
+                  let intersection, intersectionArea;
+                  let intersectionRatio = 1;
+                  try {
+                    intersection = intersect(feature.geometry, hit.geometry);
+                    if (intersection) {
+                      intersectionArea = area(intersection);
+                    } else {
+                      intersectionArea = 0;
+                    }
+                    intersectionRatio = intersectionArea / featureArea;
+                  } catch (e) {
+                    console.log("error during intersection ", e);
+                    console.log("error during intersection ", intersection);
+                  }
+
+                  if (intersectionRatio > 0.1) {
+                    overlapping.push({ id: hit.properties.id, kampagne: hit.properties.kampagne });
+                  } else {
+                    // console.log("no hit for ", hit.text);
+                  }
+                }
+              }
+
+              // result.data[i].geojson = buffered.geometry;
+              result.data[i].overlapping = overlapping;
+              ++i;
+            }
+            // console.timeEnd("xxx sibblingsCheck");
+
             setItems(result.data);
             setMetaInformation({ time });
           },
@@ -51,33 +97,9 @@ const FC = ({ jwt, setJWT, setLoginInfo }) => {
     <FeatureCollection
       featureClickHandler={(event) => {
         event.originalEvent.stopImmediatePropagation();
-        //console.log("yyy featureClick", event, new Error("StackTrace"));
-        const nonce = Math.round(Math.random() * 100);
-        // console.re.log(
-        //   nonce + ":yyy event.originalEvent._simulated",
-        //   event.originalEvent._simulated
-        // );
-        // console.log("yyy event.targetTouches", event.originalEvent.targetTouches);
-        // if (!event.originalEvent._simulated) {
         const feature = event.target.feature;
-
         if (feature.selected === true) {
-          // const before = JSON.stringify(routedMapRef.leafletMap.leafletElement.getBounds());
-
-          // const checkAfterBoundsChanged = (e) => {
-          //   console.log("e", e);
-
-          //   const after = JSON.stringify(routedMapRef.leafletMap.leafletElement.getBounds());
-          //   if (after === before) {
-
-          //   }
-          // };
-          // routedMapRef.leafletMap.leafletElement.on("moveend", checkAfterBoundsChanged);
-          // // routedMapRef.leafletMap.leafletElement.on("zoomend", checkAfterBoundsChanged);
           zoomToFeature(event.target.feature);
-
-          // routedMapRef.leafletMap.leafletElement.off("moveend", checkAfterBoundsChanged);
-          // // routedMapRef.leafletMap.leafletElement.off("zoomend", checkAfterBoundsChanged);
         }
         // }
       }}
