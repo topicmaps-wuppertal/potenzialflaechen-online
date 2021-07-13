5477fd76139ece764a7f01ab12844ef8ca0ecfeb
helllth <th@cismet.de>
2021-07-13 09:46:13 +0200

    introduce a function to create the features out of the pf items (actions link not included for dependency reasons)

---
+++ src/utils/convertItemToSimpleFeature.js
@@ -0,0 +1,40 @@
+import { convertBBox2Bounds } from "react-cismap/tools/gisHelper";
+
+const convert = (_item) => {
+  const item = JSON.parse(JSON.stringify(_item));
+
+  const geometry = item?.geojson;
+  const selected = false;
+  const type = "Feature";
+  const text = item?.bezeichnung;
+
+  item.color = item?.kampagne?.color;
+
+  const info = {
+    header: item?.kampagne?.bezeichnung,
+    title: text,
+    subtitle:
+      "<html><div><b>Nummer:</b> " +
+      item?.nummer +
+      "</div><div><b>Größe:</b> " +
+      parseInt(item?.groesse, 10).toLocaleString() +
+      " m²</div></html>",
+  };
+  item.info = info;
+
+  return {
+    id: item.id,
+    text,
+    type,
+    selected,
+    geometry,
+    crs: geometry?.crs || {
+      type: "name",
+      properties: {
+        name: "urn:ogc:def:crs:EPSG::25832",
+      },
+    },
+    properties: item,
+  };
+};
+export default convert;
