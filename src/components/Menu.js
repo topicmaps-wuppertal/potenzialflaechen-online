29a3d97c29cbeaa346777c81ec837246cb274f1a
helllth <th@cismet.de>
2021-09-21 13:04:23 +0200

    added a small background help text

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -80,7 +80,31 @@ const MyMenu = () => {
     setFilterMode("kampagnen");
   }
   const topicMapTitle = "Potenzialflächen Online";
-  const simpleHelp = undefined;
+  const simpleHelp = {
+    content: `Nach § 200 Baugesetzbuch können Gemeinden bebaubare Flächen in Karten und Listen erfassen 
+(Baulandkataster). Im Rahmen des Förderprogramms Digitale Modellregionen konnte im Jahr 2021 das **Potenzialflächenkataster** 
+der Abteilung 101.1 Stadtentwicklung in das Wuppertaler Navigations- und Datenmanagementsystem WuNDa integriert und der 
+Aufbau eines Baulückenkatasters gestartet werden. Potenzialflächen sind Flächen ab 2000 m², Baulücken haben eine Größe von 400 – 2000 m².
+
+Auf der Basis der eingepflegten digitalisierten und georeferenzierten Flächeninformationen, die laufend aktualisiert werden, 
+wurde die mobile Anwendung „Potenzialflächen Online“ entwickelt. Sie erlaubt es, auf verschiedenen Endgeräten 
+ortsunabhängig auf die Datenbank und die kartographischen Darstellungen (= Kataster) zuzugreifen.
+  
+Neben der räumlichen Abgrenzung und Lage können alle **Informationen** zu Gewerbe- und Wohnbauflächenpotenzialen, 
+allen Brachflächen und weiteren Flächensammlungen im ganzen Stadtgebiet oder in ausgewählten Bezirken bzw. Quartieren 
+gesucht und angezeigt werden. Auf online lesbaren Datenblättern oder in pdf-Dateien werden alle relevanten Daten der 
+einzelnen Flächen angezeigt: 
+**Flächennummer** (diese beginnt immer mit der Stadtbezirksnummer), **Bezeichnung** (Name der Fläche), Größe (in m² und gerundet in ha), 
+**Stadtbezirk/e**, **Quartier/e**, betroffene **Flurstück/e**, **Eigentümerkategorie/n** (anonymisiert), **Lagebewertungen** 
+(bezogen auf den Verkehr und auf den Siedlungsraum), **ÖPNV-Qualität** (Schienen- und Busanbindung), **Nähe zu Verkehrswegen** (Autobahn bis Fahrradtrasse), 
+**Topographie** (mit Angabe der Hangrichtung), **Planungsrecht** (Angaben zum Regionalplan, Flächennutzungsplan, Bebauungsplänen), **Umgebungsnutzungen**, 
+vorhandene **Nutzung**, **Bebauung** und **Versiegelung**, **Wohnlagen** (gem. Gutachterausschuss), **Bodenrichtwert/e**, **Beschreibungen** 
+(Historie, Informationen, Planungen) und **Bewertungen** (Innen- oder Außenentwicklung, Potenzialarten, Restriktionen, Empfohlene Nutzung, 
+  Entwicklungsaussichten, Verfügbarkeit, Verwertbarkeit, Revitalisierung, Handlungsdruck, Handlungspriorität). 
+  
+Anmerkungen und Hinweise können an  [christiane.classen@stadt.wuppertal.de](mailto:christiane.classen@stadt.wuppertal.de) 
+(R 101.11 Intergierte Stadtentwicklung und regionale Kooperation) gesendet werden.`,
+  };
 
   const getFilterHeader = () => {
     const count = filteredItems?.length || 0;
