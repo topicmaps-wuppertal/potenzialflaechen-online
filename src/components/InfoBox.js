a108cc3b535e351f3348731d04bd859a9f2adb89
Thorsten Hell <thorsten.hell@cismet.de>
2023-04-19 08:31:41 +0200

    fix infobox responsiveness problem

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -6,14 +6,26 @@ import {
 import GenericInfoBoxFromFeature from "react-cismap/topicmaps/GenericInfoBoxFromFeature";
 import InfoBoxHeader from "react-cismap/topicmaps/InfoBoxHeader";
 import itemFilterFunction from "../utils/filterFunction";
+import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
 const InfoBox = (props) => {
   const featureCollectionContext = useContext(FeatureCollectionContext);
-  const { setSelectedFeatureByPredicate } = useContext(FeatureCollectionDispatchContext);
+  const { setSelectedFeatureByPredicate } = useContext(
+    FeatureCollectionDispatchContext
+  );
   const { selectedFeature, filterState } = featureCollectionContext;
-
+  const { responsiveState, gap, windowSize, infoBoxPixelWidth } = useContext(
+    ResponsiveTopicMapContext
+  );
   const overlappingHeaders = [];
   let counter = 1;
   const isActive = itemFilterFunction({ filterState });
+  let actualWidth = 350;
+
+  if (responsiveState === "normal") {
+    actualWidth = infoBoxPixelWidth;
+  } else if (responsiveState === "small") {
+    actualWidth = windowSize.width - gap;
+  }
 
   if (selectedFeature?.properties?.overlapping) {
     for (const overlap of selectedFeature.properties.overlapping) {
@@ -21,7 +33,7 @@ const InfoBox = (props) => {
         overlappingHeaders.push(
           <div
             style={{
-              width: 350,
+              width: actualWidth,
               paddingBottom: 3,
               paddingLeft: counter * 5,
 
@@ -46,7 +58,10 @@ const InfoBox = (props) => {
 
   return (
     <div>
-      <GenericInfoBoxFromFeature {...props} secondaryInfoBoxElements={overlappingHeaders} />
+      <GenericInfoBoxFromFeature
+        {...props}
+        secondaryInfoBoxElements={overlappingHeaders}
+      />
     </div>
   );
 };
